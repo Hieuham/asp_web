9d358b046c444ca5fd5a2b223e7797ce93fcc14e
Hieuham <ngtrunghieu010404@gmail.com>
2024-09-08 09:09:58 +0700

    update

---
+++ BTVN02/BTVN02/Controllers/Tuan02Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTVN02.Controllers
+{
+    public class Tuan02Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Hoten = "Nguyen Trung Hieu";
+            ViewBag.MSSV = "1822040136";
+            ViewBag.Nam = "Nam 3";
+            return View();
+        }
+    }
+}
