1771e72a07cc7cf327c14e2566b38942ba1addd4
Hieuham <ngtrunghieu010404@gmail.com>
2024-09-08 09:06:18 +0700

    Update

---
+++ BaiTap05/BaiTap05/Controllers/TheLoaiController.cs
@@ -0,0 +1,27 @@
+﻿using BaiTap05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap05.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Ngay = "Ngày 14";
+            ViewBag.Thang = "Tháng 4";
+            ViewData["Nam"] = "Năm 2034";
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            var theloai = new TheLoaiViewModel
+            {
+                Id = 1,
+                Name = "Trinh thám"
+            };
+
+            return View(theloai);
+        }
+    }
+}
