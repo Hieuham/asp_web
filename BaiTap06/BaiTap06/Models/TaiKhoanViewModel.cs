a76cfba34995562258b97a6708132486c619b3d5
Hieuham <ngtrunghieu010404@gmail.com>
2024-09-10 15:03:32 +0700

    update

---
+++ BaiTap06/BaiTap06/Models/TaiKhoanViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BaiTap06.Models
+{
+    public class TaiKhoanViewModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Hoten { get; set; }
+        public int Tuoi {  get; set; }
+    }
+}
---
+++ BaiTapKiemTra01/BaiTapKiemTra01/Controllers/TaiKhoanController.cs
@@ -0,0 +1,36 @@
+﻿using BaiTapKiemTra01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapKiemTra01.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DangKy(TaiKhoanViewModel model)
+        {
+            if (model.Username != null)
+            {
+                return Content(model.Username);
+            }
+
+            return View();
+
+        }
+        public IActionResult Baitap02()
+        {
+            {
+                var sanpham = new SanPhamViewModel()
+                {
+                    TenSanPham = "cà phê",
+                    
+                  
+                };
+                return View(sanpham);
+            }
+        }
+    }
+}
